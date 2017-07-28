d1040502a447f0c6065f0f7147de731ed7270639
Grace-luning-fu <grace.luning.fu@gmail.com>
2017-07-28 14:51:39 -0400

    add comments and new features

--- src/invoice/InvoiceApp.java
+++ src/invoice/InvoiceApp.java
@@ -7,6 +7,12 @@ public class InvoiceApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/**
+		 * This is a program to produce accepts a tax rate and a list of 
+		 * products (with prices and descriptions), and outputs an invoice.
+		 * 
+		 * @author Grace
+		 **/
 		String contin = null;
 		double taxRate;
 		double totalAmount = 0.0;
@@ -15,52 +21,65 @@ public class InvoiceApp {
 		ArrayList<Product> arr = new ArrayList<Product>();
 
 		System.out.println("Input a product tax:");
-		Scanner scprice = new Scanner(System.in);
-		taxRate=scprice.nextDouble();
-		System.out.println(taxRate);
+		Scanner sctax = new Scanner(System.in);
+		taxRate=sctax.nextDouble();
 		
 		do{
 		
+        // Create a new instance of Product as an item in ArrayList
 		Product one = new Product();
-
+        
+		//Prompt user input for name
 		System.out.println("Input a product name:");
 		Scanner scname = new Scanner(System.in);
 			
 		String name = scname.nextLine();
 		one.setName(name);
-		System.out.println(one.getName());
 
+		//Prompt user input for description
+		System.out.println("Input a product description:");
+		Scanner scde = new Scanner(System.in);
+			
+		String des = scde.nextLine();
+		one.setDescription(des);
+		
+		//Prompt user input for price
 		System.out.println("Input a product price:");
 		Scanner scpri = new Scanner(System.in);
 		
 		Double price = scpri.nextDouble();
 		one.setPrice(price);
-		System.out.println(one.getPrice());
-	
+	    
+		// prompt for choice of continue or not 
 		System.out.println("Would you like to continue? y/n");
 		Scanner scan = new Scanner(System.in);
 		contin= scan.nextLine(); 
 		
+		//add this item in to the ArrayList
 		arr.add(one);
 		
 		} while (contin.equalsIgnoreCase("y"));
-		
-		// arr.addProduct();
 
+		//print out the invoice
 		System.out.println("Invoice");
 		
+		// Enhanced loop over the ArrayList arr, add each price to total amount
 		for (Product item: arr) {
 			totalAmount += item.getPrice(); 
-			System.out.println(item.getName() + "   " + item.getPrice());
+			System.out.println(item.getName() + "    " + item.getDescription() + "    " + "$" + item.getPrice());
 
 		}
 		
+		//Calculate totalAmount, tax and finalAmount
 		tax = totalAmount * taxRate/100;
 		finalAmount = totalAmount + tax; 
 		
-		System.out.println(totalAmount);
-		System.out.println(tax);
-		System.out.println(finalAmount);
+		//Print out the invoice, taxRate, tax (round to two decimal places)
+		System.out.println("Total amount: $" + totalAmount);
+		System.out.println("Tax rate: " + taxRate + "%");
+		System.out.printf("Tax: $%.2f", tax);
+		System.out.printf("\nFinal amount: $%.2f",finalAmount);
+		//System.out.printf("Final amount: $"+Math.round(finalAmount,2));
 
 	}
 }
--- src/invoice/Product.java
+++ src/invoice/Product.java
@@ -2,7 +2,9 @@ package invoice;
 
 public class Product {
 	
+	// Initiate private variable name and price 
 	private String name; 
+	private String description;
 	private double price; 
 	
 
@@ -14,6 +16,17 @@ public class Product {
     	return name;
     	
     }
+    
+    
+    public void setDescription(String description){
+    	this.description = description;
+    }
+    
+    public String getDescription(){
+    	return description;
+    	
+    }
+    
     
     public void setPrice(double price){
     	this.price=price;
